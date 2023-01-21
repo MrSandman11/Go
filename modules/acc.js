e19335088d896344e615e9e4e58ce9c8b08504d9
Artem Nikitin <nikitin1111@inbox.ru>
2023-01-21 11:22:15 +0300

    Homework_ready_6_3

---
+++ modules/acc.js
@@ -0,0 +1,59 @@
+const items = document.querySelectorAll('.faq__list-item');
+const buttons = document.querySelectorAll('.faq__list-item-question-button');
+const textWrapper = document.querySelectorAll('.faq__list-item-text-wrapper');
+
+const removeActive = (i) => {
+  items[i].classList.remove('faq__list-item_active');
+  textWrapper[i].classList.remove('faq__list-item-text-wrapper_active');
+
+  if (buttons[i].classList
+      .contains('faq__list-item-question-button_odd') ||
+      buttons[i].classList
+          .contains('faq__list-item-question-button_odd_active')) {
+    buttons[i].classList
+        .remove('faq__list-item-question-button_odd_active');
+    buttons[i].classList.add('faq__list-item-question-button_odd');
+  } else {
+    buttons[i].classList
+        .remove('faq__list-item-question-button_even_active');
+    buttons[i].classList.add('faq__list-item-question-button_even');
+  }
+};
+
+buttons.forEach((btn, index) => {
+  btn.addEventListener('click', () => {
+    for (let i = 0; i < items.length; i += 1) {
+      if (index === i) {
+        textWrapper[i].classList.toggle('faq__list-item-text-wrapper_active');
+        items[i].classList.toggle('faq__list-item_active');
+
+        if (buttons[i].classList
+            .contains('faq__list-item-question-button_odd') ||
+            buttons[i].classList
+                .contains('faq__list-item-question-button_odd_active')) {
+          buttons[i].classList
+              .toggle('faq__list-item-question-button_odd_active');
+          buttons[i].classList.toggle('faq__list-item-question-button_odd');
+        } else {
+          buttons[i].classList
+              .toggle('faq__list-item-question-button_even_active');
+          buttons[i].classList.toggle('faq__list-item-question-button_even');
+        }
+      } else {
+        removeActive(i);
+      }
+    }
+  });
+});
+
+document.addEventListener('click', ({target}) => {
+  items.forEach(elem => {
+    if (elem.classList.contains('faq__list-item_active')) {
+      if (!target.closest('.faq__list-item')) {
+        for (let i = 0; i < items.length; i += 1) {
+          removeActive(i);
+        }
+      }
+    }
+  });
+});
---
+++ modules/modal.js
@@ -0,0 +1,22 @@
+const buttonCall = document.querySelector('.header__button');
+const modalOverlay = document.querySelector('.modal-overlay');
+
+const modalControl = (modalOverlay, buttonCall) => {
+  const openModal = () =>
+    modalOverlay.classList.add('modal-overlay_active');
+
+  const closeModal = () =>
+    modalOverlay.classList.remove('modal-overlay_active');
+
+  buttonCall.addEventListener('click', openModal);
+
+  modalOverlay.addEventListener('click', e => {
+    const target = e.target;
+    if (target === modalOverlay ||
+      target.closest('.modal__close')) {
+      closeModal();
+    }
+  });
+};
+
+modalControl(modalOverlay, buttonCall);
